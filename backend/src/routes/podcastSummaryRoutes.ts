5cd252bc4d2c612dc26ee334f400f9c7e95723f5
Fahad Khan <kanlifahad@gmail.com>
2025-04-16 10:50:32 -0500

    update import

--- backend/src/routes/podcastSummaryRoutes.ts
+++ backend/src/routes/podcastSummaryRoutes.ts
@@ -1,5 +1,5 @@
 import express, { Request, Response, Router } from 'express';
-import { PodcastSummary } from '../types/Podcast';
+import { PodcastSummary } from '../types/PodcastSummary';
 
 const router: Router = express.Router();
 
