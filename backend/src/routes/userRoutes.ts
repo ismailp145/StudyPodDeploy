be4d7f7f4ae5e9e3f9c1bfe2449dd455b12e15c2
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2025-04-27 12:29:06 -0500

    crud complete for users

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,32 +1,61 @@
 import express, { Request, Response, Router } from 'express';
 import {User} from '../types/User';
+import { PrismaClient } from '../generated/prisma';
 
 const router: Router = express.Router();
+const prisma = new PrismaClient();
 
-let users: User[] = [
-  { id: 1, name: 'Alice', email: 'alice@example.com' },
-  { id: 2, name: 'Bob', email: 'bob@test.org' },
-  { id: 3, name: 'Charlie', email: 'charlie@something.net' },
-];
-
-router.get('/', (req: Request, res: Response) => {
-  res.json(users);
+router.get('/', async (req: Request, res: Response): Promise<void> => {
+  try {
+    const user = await prisma.user.findMany();
+    res.json({ user });
+  } catch (error) {
+    console.error('Error fetching podcasts:', error);
+    res.status(500).json({ error: 'Failed to retrieve podcasts' });
+  }
 });
 
-router.post('/', (req: Request, res: Response) => {
-  const newUser: User = {
-    id: Date.now(), // simplistic unique ID
-    name: req.body.name,
-    email: req.body.email,
-  };
-  users.push(newUser);
-  res.status(201).json(newUser);
+
+router.post('/', async (req: Request, res: Response): Promise<void> => {
+  const {email, password} = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({ error: 'Email and password are Required' });
+    return;
+  }
+
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        email,
+        password
+      },
+    });
+
+    res.status(201).json({ user: newUser });
+  } catch (error) {
+    console.error('Error creating new user:', error);
+    res.status(500).json({ error: 'Failed to create user' });
+  }
 });
 
-router.delete('/:id', (req: Request, res: Response) => {
-  const userId = parseInt(req.params.id, 10);
-  users = users.filter((user) => user.id !== userId);
-  res.status(204).send();
+router.delete('/:id', async (req: Request, res: Response): Promise<void> => {
+  const id = req.params.id;
+
+  try {
+    const deletedUser = await prisma.user.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    res.json({ message: 'User deleted', deletedUser });
+  } catch (error) {
+    console.error('Error deleting User:', error);
+    res
+      .status(404)
+      .json({ error: 'User not found or could not be deleted' });
+  }
 });
 
 export default router;
