278de2370aad9da79c93b1c0a5e39f669123f830
Ismail Patel <100822861+ismailp145@users.noreply.github.com>
2025-06-11 12:44:46 -0400

    updated generic keywords

--- backend/services/keywordService.ts
+++ backend/services/keywordService.ts
@@ -10,9 +10,11 @@ const as = nlp.as;
 
 
 export function filterKeywords(text:string){
+  // Common nouns that are not relevant to the podcast
   const generic = new Set([
-    'difference','compare','comparison','podcast', 
-    'make', 'generate', 'you', 'can', 'produce', 'me',
+    'difference','comparison','podcast', 
+    'make', 'generate', 'you', 'can', 'produce', 'me', 'explanation', 'topic', 
+    'topic', 'subject', 'matter', 'he', 'him', 'her', 'they', 'them', 'their', 'we', 'our'
   ]);
   return winkNLP(model)
          .readDoc(text)
